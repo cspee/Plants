e981ce712af5b8e7c1159ae8a582b64cfcf3ea78
cspee <peps.cspe@yandex.ru>
2024-09-05 18:55:58 +0300

    .

--- script.js
+++ script.js
@@ -4,29 +4,51 @@ const cards = document.querySelectorAll('.cards')
 for (let i = 0; i < btns.length; i++) {
 
     btns[i].addEventListener("click", () => {
-
+        let countBlur = Array.from(btns).filter(x => x.classList.contains('blurred')).length
+        console.log(countBlur);
+            if (countBlur === 0 ) {
+                for (let j = 0; j < btns.length; j++) {
+                    if ( j !== i) {
+                        btns[j].classList.toggle('blurred')
+                    }
+                }
+            } else if (countBlur === 2 && btns[i].classList.contains('blurred')) {
+                btns[i].classList.toggle('blurred')
+            } else if (countBlur === 1 && !btns[i].classList.contains('blurred')) {
+                btns[i].classList.toggle('blurred')
+            } else if (countBlur === 2 && !btns[i].classList.contains('blurred')) {
+                for (let j = 0; j < btns.length; j++) {
+                    if ( j !== i) {
+                        btns[j].classList.toggle('blurred')
+                    }
+                }
+            }
+        
+        console.log(countBlur);
 
         let countActive = document.querySelectorAll('.activeBtnFourPage').length
+
         if (countActive == 0) {
             for (let j = 0; j < cards.length; j++) {
 
                 if (btns[i].dataset.category != cards[j].dataset.category) {
-                    cards[j].classList.toggle("blurred")
+
+                    cards[j].classList.toggle("blurred");
                 }
             }
-            btns[i].classList.toggle('activeBtnFourPage')
-        } 
-        
+            btns[i].classList.toggle('activeBtnFourPage');
+        }
+
         else if (countActive == 1 && btns[i].classList.contains('activeBtnFourPage')) {
             for (let j = 0; j < cards.length; j++) {
                 if (btns[i].dataset.category != cards[j].dataset.category) {
-                    cards[j].classList.toggle("blurred")
+                    cards[j].classList.toggle("blurred");
                 }
             }
             btns[i].classList.toggle('activeBtnFourPage')
         }
 
-        else if (countActive == 1 && !btns[i].classList.contains('activeBtnFourPage') ) {
+        else if (countActive == 1 && !btns[i].classList.contains('activeBtnFourPage')) {
 
             for (let j = 0; j < cards.length; j++) {
                 if (btns[i].dataset.category == cards[j].dataset.category) {
@@ -34,7 +56,7 @@ for (let i = 0; i < btns.length; i++) {
                 }
             }
             btns[i].classList.toggle('activeBtnFourPage')
-        } else if (countActive == 2  && btns[i].classList.contains('activeBtnFourPage')) {
+        } else if (countActive == 2 && btns[i].classList.contains('activeBtnFourPage')) {
             for (let j = 0; j < cards.length; j++) {
                 if (btns[i].dataset.category == cards[j].dataset.category) {
                     cards[j].classList.toggle("blurred")
@@ -43,10 +65,8 @@ for (let i = 0; i < btns.length; i++) {
             btns[i].classList.toggle('activeBtnFourPage')
         }
 
-        console.log(chekcountActive);
     })
 
-    console.log('');
 }
 
 const navMenu = document.querySelector('.navMenu');
@@ -54,28 +74,39 @@ const ulList = document.querySelector('.ulList')
 const burgerContainer = document.querySelector('.burgerContainer')
 const logoContainer = document.querySelector('.logoContainer')
 
-function burgerMenu() {
-    const width = window.innerWidth;
-    // console.log(`Ширина экрана: ${width}px`); 
+document.addEventListener('DOMContentLoaded', function() {
+    const burgerContainer = document.querySelector('.burgerContainer');
+    const navMenu = document.querySelector('.navMenu');
 
-    if (width < 640) {// ширина здесь не такая же как в отладчике браузера
-        // console.log('Меню скрыто');
-        navMenu.classList.add('hidden'); 
-        burgerContainer.classList.remove('hidden')
-    } else {
-        // console.log('Меню отображается');
-        burgerContainer.classList.add('hidden')
-        navMenu.classList.remove('hidden'); 
-        logoContainer.classList.remove('hidden')
-        ulList.style.fontSize = '16px'; 
-    }
-}
+    burgerContainer.addEventListener('click', function() {
+        navMenu.classList.toggle('active'); // Переключить класс active
+    });
+});
 
-burgerContainer.addEventListener('click', () => {
-    navMenu.classList.toggle('hidden')
-    logoContainer.classList.toggle('hidden')
-    ulList.style.fontSize = '10px'; 
-})
+// function burgerMenu() {
+//     const width = window.innerWidth;
+//     // console.log(`Ширина экрана: ${width}px`); 
 
-window.addEventListener('resize', burgerMenu);  
-burgerMenu();
+//     if (width < 640) {// ширина здесь не такая же как в отладчике браузера
+//         // console.log('Меню скрыто');
+//         navMenu.classList.add('hidden');
+//         burgerContainer.classList.remove('hidden')
+//     } else {
+//         // console.log('Меню отображается');
+//         burgerContainer.classList.add('hidden')
+//         navMenu.classList.remove('hidden');
+//         logoContainer.classList.remove('hidden')
+//         ulList.style.fontSize = '16px';
+//     }
+// }
+
+// burgerContainer.addEventListener('click', () => {
+//     navMenu.classList.toggle('hidden')
+//     logoContainer.classList.toggle('hidden')
+//     ulList.style.fontSize = '10px';
+// })
+
+
+
+// window.addEventListener('resize', burgerMenu);
+// burgerMenu();
--- scriptFourPage.js
+++ scriptFourPage.js
@@ -8,56 +8,105 @@
 //     });
 
 const basicBtn = document.getElementById('basicsBtn')
-const standardBtn  = document.getElementById('standardBtn')
-const proCareBtn  = document.getElementById('proCareBtn')
+const standardBtn = document.getElementById('standardBtn')
+const proCareBtn = document.getElementById('proCareBtn')
 
-const fBtnsContainer =document.querySelector('.fBtnsContainer')
+const fBtnsContainer = document.querySelector('.fBtnsContainer')
 
 const pricesBlock = document.getElementById('pricesBlock')
 const pricesBlockStand = document.getElementById('pricesBlockStand')
 const pricesBlockPro = document.getElementById('pricesBlockPro')
 
 
+let activeBlock = null
+let activeBtn = null
 
 
+function showBlock(btn, block) {
+    if (!fBtnsContainer.querySelector(".pricesBlock")) {
+        block.classList.remove('hidden')
+        btn.replaceWith(block)
+        fBtnsContainer.style.gap = '5px'
+        activeBlock = block
+        activeBtn = btn
+        console.log("РРАБОТА")
+    }
+    //почемеу НЕ ПОДСКАЗЫВАЕТ МЕТОДЫ КОГДа пишу contains нету его в подсказках btn.classList.contains
+    else if (fBtnsContainer.querySelector(".pricesBlock") && btn.classList.contains("styleForElemInButton")) {
+        console.log('ДОМ')
+        showBtn(activeBtn, activeBlock)
+        showBlock(btn, block)
+    }
 
+}
 
-basicBtn.addEventListener('click', () => {
-    pricesBlock.classList.remove('hidden')
-    basicBtn.replaceWith(pricesBlock)
-    fBtnsContainer.style.gap = '5px'
-})
+function showBtn(btn, block) {
 
-pricesBlock.addEventListener('click', () => {
-    pricesBlock.replaceWith(basicBtn)
+    block.replaceWith(btn)
     fBtnsContainer.style.gap = '32px'
-})
-// вторая кнопка
-standardBtn.addEventListener('click', () => {
-    standardBtn.classList.add('hidden')
-    pricesBlockStand.classList.remove('hidden')
-    standardBtn.replaceWith(pricesBlockStand)
-    fBtnsContainer.style.gap = '5px'
-})
+}
 
-pricesBlockStand.addEventListener('click', () => {
-    standardBtn.classList.remove('hidden')
-    pricesBlockStand.classList.add('hidden')
-    pricesBlockStand.replaceWith(standardBtn)
-    fBtnsContainer.style.gap = '32px'
-})
-// третья кнопка
 
-proCareBtn.addEventListener('click', () => {
-    proCareBtn.classList.add('hidden')
-    pricesBlockPro.classList.remove('hidden')
-    proCareBtn.replaceWith(pricesBlockPro)
-    fBtnsContainer.style.gap = '5px'
-})
 
-pricesBlockPro.addEventListener('click', () => {
-    proCareBtn.classList.remove('hidden')
-    pricesBlockPro.classList.add('hidden')
-    pricesBlockPro.replaceWith(proCareBtn)
-    fBtnsContainer.style.gap = '32px'
-})
+basicBtn.addEventListener('click', () => showBlock(basicBtn, pricesBlock))
+standardBtn.addEventListener('click', () => showBlock(standardBtn, pricesBlockStand))
+proCareBtn.addEventListener('click', () => showBlock(proCareBtn, pricesBlockPro))
+
+pricesBlock.addEventListener('click', () => showBtn(basicBtn, pricesBlock))
+pricesBlockStand.addEventListener('click', () => showBtn(standardBtn, pricesBlockStand))
+pricesBlockPro.addEventListener('click', () => showBtn(proCareBtn, pricesBlockPro))
+
+
+
+// basicBtn.addEventListener('click', () => {
+//     if (!fBtnsContainer.querySelector(".pricesBlock")) {
+//         pricesBlock.classList.remove('hidden')
+//         basicBtn.replaceWith(pricesBlock)
+//         fBtnsContainer.style.gap = '5px'
+//     }
+
+
+
+// },)
+
+// pricesBlock.addEventListener('click', () => {
+//     pricesBlock.replaceWith(basicBtn)
+//     fBtnsContainer.style.gap = '32px'
+// })
+// // вторая кнопка
+// standardBtn.addEventListener('click', () => {
+//     if (!fBtnsContainer.querySelector(".pricesBlock")) {
+//         standardBtn.classList.add('hidden')
+//         pricesBlockStand.classList.remove('hidden')
+//         standardBtn.replaceWith(pricesBlockStand)
+//         fBtnsContainer.style.gap = '5px'
+//     }
+
+// })
+
+// pricesBlockStand.addEventListener('click', () => {
+//     standardBtn.classList.remove('hidden')
+//     pricesBlockStand.classList.add('hidden')
+//     pricesBlockStand.replaceWith(standardBtn)
+//     fBtnsContainer.style.gap = '32px'
+// })
+// // третья кнопка
+
+// proCareBtn.addEventListener('click', () => {
+
+//     if (!fBtnsContainer.querySelector(".pricesBlock")) {
+//         proCareBtn.classList.add('hidden')
+//         pricesBlockPro.classList.remove('hidden')
+//         proCareBtn.replaceWith(pricesBlockPro)
+//         fBtnsContainer.style.gap = '5px'
+
+//     }
+
+// })
+
+// pricesBlockPro.addEventListener('click', () => {
+//     proCareBtn.classList.remove('hidden')
+//     pricesBlockPro.classList.add('hidden')
+//     pricesBlockPro.replaceWith(proCareBtn)
+//     fBtnsContainer.style.gap = '32px'
+// })
